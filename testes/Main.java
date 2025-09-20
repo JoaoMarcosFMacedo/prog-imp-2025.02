59f20db4315a48d249ace37d88779e138102878c
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2025-09-20 00:38:06 +0000

    falta terminar

---
+++ testes/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        //Vamos fazer um teste sobre matriz
+        System.out.print("Digite um número para servir de base para as Matrizes");
+        int n = scanner.nextInt();
+        for ( int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < n ; j++){
+                C [i] [j] = [i][j] + [i][j]; 
+            }
+        }
+
+
+
+    }  
+    // public static int somaMatris{   
+
+        
+    }
+
+}
