e35683cd555c53341752b43befdd800e78ed35f1
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2025-12-13 12:53:28 +0000

    commit 13 12 2025

--- Objeto/ferias2025/FolhaPagamento.java
+++ Objeto/ferias2025/FolhaPagamento.java
@@ -2,4 +2,8 @@ package Objeto.ferias2025;
 
 public class FolhaPagamento {
     int idFuncionario;
+    String nomeFuncionario;
+    int endrecoCep;
+    double ganhoPorHora;
+    double impostos;
 }
